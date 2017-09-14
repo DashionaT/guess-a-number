4202a64a66ccb14cb02c0668c47ab5a5f71869b0
Todd <dtodd5092@greenville.k12.sc.us>
2017-09-14 09:24:33 -0400

    Made the aliens shoot more accurately

--- guess_a_number.py
+++ guess_a_number.py
@@ -3,12 +3,14 @@ import random
 # config
 low = 1
 high = 100
+limit = 5
 rand = random.randint(low, high)
 print("I'm thinking of a number from " + str(low) + " to " + str(high) + ".")
 
 guess = -1
+tries = 0
 
-while guess != rand:
+while guess != rand and tries < limit:
     guess = input("Take a guess: ")
     guess = int(guess)
     
@@ -16,7 +18,12 @@ while guess != rand:
         print("You guessed too low.")
     elif guess > rand:
         print("You guessed too high.")
-    else:
-        print("You got it!")
 
-print("Game over")
+    tries += 1
+
+# tell player out come
+if guess == rand:
+    print ("You Win!")
+else:
+    print ("You suck.... Take your L... The number I was thinking of was " + str(rand) + ".")
+    

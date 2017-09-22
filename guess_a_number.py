6661a271eeea6645eaaca47fb0782ec228151b02
Todd <dtodd5092@greenville.k12.sc.us>
2017-09-22 09:34:51 -0400

    added stuff

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,15 +1,20 @@
 import random
+import math
 
 # config
 low = 1
-high = 10
-limit = 4
+high = 100
+limit = math.ceil(math.log(high - low + 1, 2))
 
 # helper functions
 def show_start_screen():
-    print("**************************")
-    print("**** Guess a Number ! ****")
-    print("**************************")
+    
+    print("  ________                                        _______               ___.                ._.")
+    print(" /  _____/ __ __   ____   ______ ______ _____     \      \  __ __  _____\_ |__   ___________| |")
+    print("/   \  ___|  |  \_/ __ \ /  ___//  ___/ \__  \    /   |   \|  |  \/     \| __ \_/ __ \_  __ \ |")
+    print("\    \_\  \  |  /\  ___/ \___ \ \___ \   / __ \_ /    |    \  |  /  Y Y  \ \_\ \  ___/|  | \/\|")
+    print(" \______  /____/  \___  >____  >____  > (____  / \____|__  /____/|__|_|  /___  /\___  >__|   __")
+    print("        \/            \/     \/     \/       \/          \/            \/    \/     \/       \/")
 
 def show_credits():
     print("This awesome game was created by Dai.")
@@ -17,7 +22,7 @@ def show_credits():
 def get_guess():
     while True:
         guess = input("Guess a number: ")
-
+        print()
         if guess.isnumeric():
             guess = int(guess)
             return guess
@@ -25,13 +30,14 @@ def get_guess():
             print("You must enter a number.")
 
 def pick_number():
-    print("I'm thinking of a number from " + str(low) + " to " + str(high) +".")
+    print("I'm thinking of a number from " + str(low) + " to " + str(high) +". You will get a total of 7 tries.")
 
     return random.randint(low, high)
 
 def check_guess(guess, rand):
     if guess < rand:
         print("You guessed too low.")
+        
     elif guess > rand:
         print("You guessed too high.")
 
@@ -44,7 +50,7 @@ def show_result(guess, rand):
 def play_again():
     while True:
         decision = input("Would you like to play again? (y/n) ")
-
+        print("")
         if decision == 'y' or decision == 'yes':
             return True
         elif decision == 'n' or decision == 'no':
